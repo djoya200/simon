8427d02108e97561075e0c303f75a394fe156185
Desiree Joya <desireejoya@gmail.com>
2021-04-15 10:35:30 -0400

    pseudo code finished ready to continue

---
+++ app.js
@@ -0,0 +1,46 @@
+//------------------psuedo code-------------------------//
+// -create buttons for start 
+// -create button for directions
+
+// when game starts:
+// -use Math.random to generate random patterns
+// -Math.floor(Math.random()*4)
+
+// This JavaScript function always returns a random number between min and max (both included):
+// W3Schools
+// Math.floor(Math.random() * (max - min + 1) ) + min;
+Math.floor(Math.random() * (4 - 1) ) + 1; 
+console.log(Math.floor(Math.random() * (4 - 1 + 1) ) + 1 )
+
+// use the array to tell which square to flash a lighter or darker color 
+// the square they are supposed to click 
+
+// when users click the buttons that square flashes and then thier 
+// answer is pushed into another array. 
+
+// -set conditionals to see  if they are ==== to 
+// eachother then they proceed to the next level
+
+// popup box says: 
+// -create pop up windows for when they proceed to the next level 
+// -have baloons or confetti for when they win
+
+// add one more color/number to the game array 
+
+// after that show the flashes again and check the answers users click again. 
+
+// -create pop up window for when they lose
+
+
+// -use .push to push random number into an array from Math.floor
+// -use .push to push answers from DOM into an array
+
+// -create input box for name
+// -create buttons for start 
+// -create button for directions
+// -create boxes for level and for points 
+
+
+// -possibly create two backgrounds one for knights and a dragon 
+// -one for princess finding her rabbit
+// -make avitars move when they recieve points
