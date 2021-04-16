8909d01ba9d449ea53330131612116c5e2131f2a
Desiree Joya <desireejoya@gmail.com>
2021-04-15 22:15:37 -0400

    working on colors flashing, fixed buttons

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+
+console.log('simon app.js')
 //------------------psuedo code-------------------------//
+
+// =============  DONE ->  ================
 // -create buttons for start 
 // -create button for directions
 
+
+
+
+// =============  HERE ->  ================
 // when game starts:
 // -use Math.random to generate random patterns
 // -Math.floor(Math.random()*4)
-
-// This JavaScript function always returns a random number between min and max (both included):
-// W3Schools
-// Math.floor(Math.random() * (max - min + 1) ) + min;
-Math.floor(Math.random() * (4 - 1) ) + 1; 
-console.log(Math.floor(Math.random() * (4 - 1 + 1) ) + 1 )
 
 // use the array to tell which square to flash a lighter or darker color 
 // the square they are supposed to click 
@@ -31,16 +33,89 @@ console.log(Math.floor(Math.random() * (4 - 1 + 1) ) + 1 )
 
 // -create pop up window for when they lose
 
-
 // -use .push to push random number into an array from Math.floor
 // -use .push to push answers from DOM into an array
-
-// -create input box for name
-// -create buttons for start 
-// -create button for directions
-// -create boxes for level and for points 
-
 
 // -possibly create two backgrounds one for knights and a dragon 
 // -one for princess finding her rabbit
 // -make avitars move when they recieve points
+
+// ====================================================
+// This JavaScript function always returns a random number between min and max (both included):
+// W3Schools
+// Math.floor(Math.random() * (max - min + 1) ) + min;
+var generatedSimonColors = []
+var userSimonColors = []
+var myVar = ''
+
+// const whenTheGameStarts() => {
+// 
+// } 
+
+const generateARandomSimonColor = () => {
+    var newColor = Math.floor(Math.random() * (4 - 1) ) + 1;
+    generatedSimonColors.push(newColor)
+}
+const gettingColorInput = (i) => {
+    console.log("User clicked " + i)
+    userSimonColors.push(i)
+}
+console.log(userSimonColors)
+// document.getElementById("one").style = "background-color: white"
+// document.getElementById("one").style = "background-color: green"
+
+
+const flashTheSimonColors = () => {
+    console.log('flashTheSimonColors')
+    for(let i = 0 ; i <= generatedSimonColors.length -1 ; i++){
+        // needs to change from origianl color to white
+        // stays white for one second
+        // then back to orginal color
+        let timeForInt = 1000
+        // let howManyTimesInIf = 1
+
+        if(generatedSimonColors[i] == 1){
+            console.log('in the green')
+            // howManyTimesInIf +=1
+            setTimeout(function(){
+
+                document.getElementById("one").style = "background-color: white"
+
+            }, timeForInt * howManyTimesInIf); 
+
+            setTimeout(function(){
+
+                document.getElementById("one").style = "background-color: green" 
+
+            }, timeForInt); 
+
+            //  document.getElementById("one").style = "background-color: green"
+        }
+        else if(generatedSimonColors[i] == 2){
+            console.log('in the red')
+           
+            setTimeout(function(){
+                document.getElementById("two").style = "background-color: white"
+                
+            }, timeForInt); 
+            setTimeout(function(){
+                        document.getElementById("two").style = "background-color: red"
+                        
+            }, timeForInt); 
+        }
+        else if(generatedSimonColors[i] == 3){
+            console.log('in the yellow')
+
+        }
+        else if(generatedSimonColors[i] == 4){
+            console.log('in the blue')
+
+        }
+    }
+
+}
+
+
+generatedSimonColors = [1, 2]
+flashTheSimonColors()
+console.log()  
