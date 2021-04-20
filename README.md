926426ce84cce2ae875bbaface1cf2446f301feb
Desiree Joya <desireejoya@gmail.com>
2021-04-19 21:51:54 -0400

    added a README.md file

---
+++ README.md
@@ -0,0 +1,13 @@
+# H1 Simon
+
+### H3 by Desiree Joya
+
+- This game was made with Javascript, HTML, and CSS
+
+- To create this project I started with a wireframe and pseudocode. I created an HTML page and styled it with CSS to resemble a classic Simon toy. I added my Javascript and began to work on the logic and functionality of a game of simon. I tested, refactored, and deployed it to Github pages. 
+
+- The link to my site is: [title](https://www.example.com)
+
+- To run the actual code separately, download the code and click run in browser from an editor. 
+
+- the robot gifs are from [title](https://www.tenor.com) artist is unknown
--- app.js
+++ app.js
@@ -1,40 +1,6 @@
 
 console.log('simon app.js')
-//------------------psuedo code-------------------------//
 
-// =============  DONE ->  ================
-// -create buttons for start 
-// -create button for directions
-
-// when game starts:
-// -use Math.random to generate random patterns
-// -Math.floor(Math.random()*4)
-
-// use the array to tell which square to flash a lighter or darker color 
-// the square they are supposed to click 
-
-// when users click the buttons that square flashes and then thier 
-// answer is pushed into another array. 
-
-// -set conditionals to see  if they are ==== to 
-// eachother then they proceed to the next level
-
-// add one more color/number to the game array 
-// after that show the flashes again and check the answers users click again. 
-
-// -use .push to push random number into an array from Math.floor
-// -use .push to push answers from DOM into an array
-
-// -create pop up window for when they lose
-//  =============  HERE ->  ================
-
-// popup box says: 
-// -have baloons or confetti for when they win
-
-// ====================================================
-// This JavaScript function always returns a random number between min and max (both included):
-// W3Schools
-// Math.floor(Math.random() * (max - min + 1) ) + min;
 var generatedSimonColors = []
 var userSimonColors = []
 var myVar = ''
@@ -44,37 +10,26 @@ const whenTheGameStarts = () => {
     flashTheSimonColors()
 }
 const checkingTheColors = () => {
-    console.log("checkingTheColors")
-    console.log(userSimonColors)
-    console.log(generatedSimonColors)
-    // take these two lines out ^
     if (userSimonColors.length === generatedSimonColors.length) {
         console.log(userSimonColors.length + " length")
         let isItWrong = false
         for (let i = 0; i < generatedSimonColors.length; i++) {
             if (userSimonColors[i] !== generatedSimonColors[i]) {
                 isItWrong = true
-                // window.alert("You Lose. Try again!")
-                // modalLoseOn()
-                // youLostThisGame
             }
         }
         console.log("isItWrong ==  " + isItWrong)
         if (isItWrong === false) {
             document.getElementById("level").innerHTML = "Level: " + generatedSimonColors.length
             document.getElementById("points").innerHTML = "Points: " + generatedSimonColors.length * 100
-
             userSimonColors = []
             generateARandomSimonColor()
             flashTheSimonColors()
-            if (generatedSimonColors.length == 4) {
-                // window.alert("YOU WIN!!!!!")
+            if (generatedSimonColors.length == 16) {
                 modalWinOn()
             } else {
                 // window.alert("Congratulations! Proceed to the next level.")
             }
-
-
         } else {
             modalLoseOn()
         }
@@ -87,7 +42,7 @@ const generateARandomSimonColor = () => {
 const gettingColorInput = (i) => {
     if (i == 1) {
         setTimeout(function () {
-            document.getElementById("one").style = "background-color: #2df726"
+            document.getElementById("one").style = "background-color: white"
         }, 500);
         setTimeout(function () {
             document.getElementById("one").style = "background-color: #a3e048"
@@ -96,25 +51,25 @@ const gettingColorInput = (i) => {
         }, 1000);
     } else if (i == 2) {
         setTimeout(function () {
-            document.getElementById("two").style = "background-color: #ff4038"
+            document.getElementById("two").style = "background-color: white"
         }, 500);
         setTimeout(function () {
             document.getElementById("two").style = "background-color: #e6261f"
             userSimonColors.push(i)
             checkingTheColors()
         }, 1000);
-    }else if (i == 3){
+    } else if (i == 3) {
         setTimeout(function () {
-            document.getElementById("three").style = "background-color: #ffe716"
+            document.getElementById("three").style = "background-color: white"
         }, 500);
         setTimeout(function () {
             document.getElementById("three").style = "background-color: #f7d038"
             userSimonColors.push(i)
             checkingTheColors()
         }, 1000);
-    } else if (i == 4){
+    } else if (i == 4) {
         setTimeout(function () {
-            document.getElementById("four").style = "background-color: #2279e3"
+            document.getElementById("four").style = "background-color: white"
         }, 500);
         setTimeout(function () {
             document.getElementById("four").style = "background-color: #4355db"
@@ -122,24 +77,14 @@ const gettingColorInput = (i) => {
             checkingTheColors()
         }, 1000);
     }
-    // console.log("User clicked " + i)
-    
 }
 const flashTheSimonColors = () => {
 
     for (let i = 0; i <= generatedSimonColors.length - 1; i++) {
-        // needs to change from origianl color to white
-        // stays white for one second
-        // then back to orginal color
-        let timeForInt = 1000
-        // let howManyTimesInIf = 1
 
         if (generatedSimonColors[i] == 1) {
-
-
             setTimeout(function () {
-
-                document.getElementById("one").style = "background-color: #2df726";
+                document.getElementById("one").style = "background-color: white";
 
             }, (((i + 1) * 2) - 1) * 1000);
 
@@ -149,13 +94,12 @@ const flashTheSimonColors = () => {
 
             }, ((i + 1) * 2 * 1000));
 
-            //  document.getElementById("one").style = "background-color: green"
         }
         else if (generatedSimonColors[i] == 2) {
 
 
             setTimeout(function () {
-                document.getElementById("two").style = "background-color: #ff4038"
+                document.getElementById("two").style = "background-color: white"
 
             }, (((i + 1) * 2) - 1) * 1000);
             setTimeout(function () {
@@ -166,7 +110,7 @@ const flashTheSimonColors = () => {
         else if (generatedSimonColors[i] == 3) {
             setTimeout(function () {
 
-                document.getElementById("three").style = "background-color: #ffe716";
+                document.getElementById("three").style = "background-color: white";
 
             }, (((i + 1) * 2) - 1) * 1000);
 
@@ -182,7 +126,7 @@ const flashTheSimonColors = () => {
 
             setTimeout(function () {
 
-                document.getElementById("four").style = "background-color: #2279e3";
+                document.getElementById("four").style = "background-color: white";
 
             }, (((i + 1) * 2) - 1) * 1000);
 
@@ -227,7 +171,6 @@ window.onclick = function (event) {
     }
 }
 modalLoseOn = () => {
-    console.log("modal lose called")
     document.getElementById("losingModal").style.display = "block"
 }
 
@@ -235,7 +178,7 @@ modalLoseOn = () => {
 const clickedTheResetButton = () => {
     generatedSimonColors = []
     userSimonColors = []
-    document.getElementById("level").innerHTML = "Level: " 
+    document.getElementById("level").innerHTML = "Level: "
     document.getElementById("points").innerHTML = "Points: "
 }
 
@@ -252,6 +195,5 @@ window.onclick = function (event) {
     }
 }
 modalWinOn = () => {
-    console.log("winning model called")
     document.getElementById("winningModal").style.display = "block"
 }
